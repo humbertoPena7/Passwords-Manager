62dad422a67bfce0574d8ad3720297cbb7b8e47c
Humberto.pd <humbertodejesus.penaduenas@gmail.com>
2026-04-21 19:48:38 -0600

    Refactor database_connection to improve error handling

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import psycopg2
 from tkinter import messagebox
 
-# Funcion para la conexion a PostgreSQL
 def database_connection():
     try:
         connection = psycopg2.connect(
@@ -11,8 +10,6 @@ def database_connection():
             password="789123"
         )
         return connection
-
     except Exception as e:
         messagebox.showerror("Error de conexión", f"No se pudo conectar a la base de datos.\n{e}")
         return None
-      
